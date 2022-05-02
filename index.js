f029552c81065478bb054aefc288ebbd3843e433
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2022-05-01 23:39:00 -0500

    variables de entorno

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
-const { PORT } = process.env;
+const { PORT, SERVER_PORT } = process.env;
 
 app.use(express.static('public'));
-app.listen(PORT, () => {
-    console.log(`Proyecto Miguel escuchando en el puerto ${ PORT }`);
-});
+app.listen(PORT, SERVER_PORT, () => {
+    console.log(`Applicacion corriendo en puerto ${ PORT }`);
+})
